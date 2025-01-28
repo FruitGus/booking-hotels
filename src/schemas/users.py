dcd8851c033fad25c5c01150536dde71b249c598
Alexandr Mekhonoshin <FruitGus@gmail.com>
2025-01-28 12:49:42 +0500

    Добавил ручку /login и создал JWT токен

--- src/schemas/users.py
+++ src/schemas/users.py
@@ -5,16 +5,22 @@ from pydantic import BaseModel, ConfigDict, EmailStr
 class UserRequestAdd(BaseModel):
     email: EmailStr
     password: str
-    first_name: str
-    last_name: str
+    first_name: str = ""
+    last_name: str = ""
+
+
+
+class UserRequestLogin(BaseModel):
+    email: EmailStr
+    password: str
+
 
 
 class UserAdd(BaseModel):
     email: EmailStr
     hashed_password: str
-    first_name: str
-    last_name: str
-
+    first_name: str = ""
+    last_name: str = ""
 
 
 class User(BaseModel):
