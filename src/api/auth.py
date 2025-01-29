74f98bdbd2a1764b7ce28d3c5e1021982d7df696
Alexandr Mekhonoshin <FruitGus@gmail.com>
2025-01-29 17:16:37 +0500

    Получение cookie пользователя из ручки и получение пользователя из cookie

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, HTTPException, Response
+from fastapi import APIRouter, HTTPException, Response, Request
+
 
 from src.database import async_session_maker
 from src.repositories.users import UsersRepository
@@ -6,24 +7,6 @@ from src.schemas.users import UserRequestAdd, UserAdd, UserRequestLogin
 from src.services.auth import AuthService
 
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
-
-@router.post("/login")
-async def login_user(
-        data: UserRequestLogin,
-        response: Response
-):
-
-    async with async_session_maker() as session:
-            user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
-            if not user:
-                raise HTTPException(status_code=401, detail="Пользователь с таким email не зарегистрирован")
-            if not AuthService().verify_password(data.password, user.hashed_password):
-                raise HTTPException(status_code=401, detail="Пароль неверный")
-            access_token = AuthService().create_access_token({"user id": user.id})
-            response.set_cookie("access_token", access_token)
-            return {"access_token": access_token}
-
-
 
 
 @router.post("/register")
@@ -45,3 +28,33 @@ async def register_user(
 
             return {"status": "OK"}
 
+
+
+@router.post("/login")
+async def login_user(
+        data: UserRequestLogin,
+        response: Response
+):
+
+    async with async_session_maker() as session:
+            user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
+            if not user:
+                raise HTTPException(status_code=401, detail="Пользователь с таким email не зарегистрирован")
+            if not AuthService().verify_password(data.password, user.hashed_password):
+                raise HTTPException(status_code=401, detail="Пароль неверный")
+            access_token = AuthService().create_access_token({"user_id": user.id})
+            response.set_cookie("access_token", access_token)
+            return {"access_token": access_token}
+
+
+
+@router.get("/only_auth")
+async def only_auth(
+        request: Request
+):
+    access_token = request.cookies.get("access_token", None)
+    data = AuthService().decode_token(access_token)
+    user_id = data["user_id"]
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_one_or_none(id=user_id)
+        return user
