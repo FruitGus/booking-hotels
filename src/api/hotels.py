4f79b18d85858fea9deb6e49b772f4764046dc43
Alexandr Mekhonoshin <FruitGus@gmail.com>
2025-02-03 10:23:53 +0500

    Начал настраивать отдельный ассинхронный контекстный менеджер, для сокращения кода

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -13,18 +13,18 @@ router = APIRouter(prefix="/hotels", tags=["Отели"])
 @router.get("", summary="Получение информации об отелях")
 async def get_hotels(
         pagination: PaginationDep,
+        db: DbDep,
         location: str | None = Query(None, description="Город и адрес"),
         title: str | None = Query(None, description="Название отеля"),
 
 ):
     per_page = pagination.per_page or 5
-    async with async_session_maker() as session:
-        return await HotelsRepository(session).get_all(
-            location=location,
-            title=title,
-            limit=per_page,
-            offset=per_page * (pagination.page - 1)
-        )
+    return await db.hotels.get_all(
+        location=location,
+        title=title,
+        limit=per_page,
+        offset=per_page * (pagination.page - 1)
+    )
 
 
     # if pagination.page and pagination.per_page:
