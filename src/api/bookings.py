3f0145f8a997eff00562115ca4179d7f449ef81a
Alexandr Mekhonoshin <FruitGus@gmail.com>
2025-02-16 17:17:11 +0500

    Добавил ручки на получение бронирований и своих бронирований

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -1,10 +1,36 @@
 from fastapi import APIRouter, Query, Body
 from datetime import date
 
-from src.api.dependencies import UserIdDep, DBDep
-from src.schemas.bookings import BookingAdd, BookingAddRequest
+from src.api.dependencies import UserIdDep, DBDep, PaginationDep
+from src.schemas.bookings import BookingAdd, BookingAddRequest, Booking
 
 router = APIRouter(prefix="/bookings", tags=["Бронирования"])
+
+
+
+@router.get("", summary="Получение всех бронирований")
+async def get_booking(
+        pagination: PaginationDep,
+        db: DBDep,
+
+):
+    per_page = pagination.per_page or 5
+    return await db.bookings.get_all(
+        limit=per_page,
+        offset=per_page * (pagination.page - 1)
+    )
+
+
+@router.get("/bookings/{user_id}", summary="Получение своих бронирований")
+async def get_booking(
+        user_id: int,
+        db: DBDep,
+):
+
+    return await db.bookings.get_all(
+        user_id = user_id
+    )
+
 
 
 
@@ -12,7 +38,12 @@ router = APIRouter(prefix="/bookings", tags=["Бронирования"])
 async def add_booking(
         user_id: UserIdDep,
         db: DBDep,
-        booking_data: BookingAddRequest,
+        booking_data: BookingAddRequest = Body(openapi_examples={
+    "1": {"summary": "Комната", "value": {
+        "title": "Отель Сочи 5 звезд у моря",
+        "location": "ул. Морячки, 3",
+    }},
+})
 ):
 
     room = await db.rooms.get_one_or_none(id=booking_data.room_id)
