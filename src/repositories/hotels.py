d9313dfd9298faf3c4ee98ed1164a31a0348b7f1
Alexandr Mekhonoshin <FruitGus@gmail.com>
2025-02-19 11:21:50 +0500

    Добавил пагинацию в get_filtered_by_time, удалил get_all, в ручке get_hotels внес измнения

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -14,14 +14,24 @@ class HotelsRepository(BaseRepository):
     model = HotelsOrm
     schema = Hotel
 
-    async def get_all(
+
+
+    async def get_filtered_by_time(
             self,
+            date_from: date,
+            date_to: date,
             location,
             title,
             limit,
             offset,
-    ) -> list[Hotel]:
-        query = select(HotelsOrm)
+    ):
+        rooms_ids_to_get = rooms_ids_for_booking(date_from=date_from, date_to=date_to)
+        hotels_ids_to_get = (
+            select(RoomsOrm.hotel_id)
+            .select_from(RoomsOrm)
+            .filter(RoomsOrm.id.in_(rooms_ids_to_get))
+        )
+        query = select(HotelsOrm).filter(HotelsOrm.id.in_(hotels_ids_to_get))
         if location:
             query = query.filter(HotelsOrm.location.ilike(f"%{location}%"))
         if title:
@@ -31,24 +41,14 @@ class HotelsRepository(BaseRepository):
             .limit(limit)
             .offset(offset)
         )
-        print(query.compile(compile_kwargs={"literal_binds": True}))
         result = await self.session.execute(query)
 
-        return [Hotel.model_validate(hotel, from_attributes=True) for hotel in result.scalars().all()]
+        hotels = result.scalars().all()
+
+        return hotels
 
 
-    async def get_filtered_by_time(
-            self,
-            date_from: date,
-            date_to: date,
-    ):
-        rooms_ids_to_get = rooms_ids_for_booking(date_from=date_from, date_to=date_to)
-        hotels_ids_to_get = (
-            select(RoomsOrm.hotel_id)
-            .select_from(RoomsOrm)
-            .filter(RoomsOrm.id.in_(rooms_ids_to_get))
-        )
-        return await self.get_filtered(HotelsOrm.id.in_(hotels_ids_to_get))
+
 
 
 
