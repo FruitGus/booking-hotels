8d68d69017a69785a592385d7ff0ecb38877139c
Alexandr Mekhonoshin <FruitGus@gmail.com>
2025-02-19 16:10:00 +0500

    Добавил facilities в ручки get и post

--- src/repositories/facilities.py
+++ src/repositories/facilities.py
@@ -1,8 +1,18 @@
 from src.repositories.base import BaseRepository
-from src.models.facilities import FacilitiesOrm
+from src.models.facilities import FacilitiesOrm, RoomsFacilitiesOrm
 from src.schemas.facilities import Facility
 
 
 class FacilitiesRepository(BaseRepository):
     model = FacilitiesOrm
     schema = Facility
+
+
+class RoomsFacility:
+    pass
+
+
+class RoomsFacilitiesRepository(BaseRepository):
+    model = RoomsFacilitiesOrm
+    schema = RoomsFacility
+
