2d10e5995e098941063c0d4b6e90cfe9973cdc6e
Alexandr Mekhonoshin <FruitGus@gmail.com>
2025-02-28 13:09:29 +0500

    Кэширование данных в ручках

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.openapi.docs import (
     get_swagger_ui_html,
@@ -8,6 +10,8 @@ import uvicorn
 import sys
 from pathlib import Path
 
+from src.init import redis_manager
+
 sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.hotels import router as router_hotels
@@ -16,8 +20,16 @@ from src.api.rooms import router as router_rooms
 from src.api.bookings import router as router_bookings
 from src.api.facilities import router as router_facilities
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    #При старте приложения
+    await redis_manager.connect()
+    yield
+    await redis_manager.close()
+    #При выключении/перегрузке приложения
 
-app = FastAPI(docs_url=None)
+
+app = FastAPI(docs_url=None, lifespan=lifespan)
 
 app.include_router(router_auth)
 app.include_router(router_hotels)
