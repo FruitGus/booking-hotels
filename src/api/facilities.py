703cdd83f43f7d6c5e51eb4e3755c90f655b19b4
Alexandr Mekhonoshin <FruitGus@gmail.com>
2025-03-03 17:36:19 +0500

    Установка Celery и создание тасок

--- src/api/facilities.py
+++ src/api/facilities.py
@@ -6,6 +6,7 @@ from src.api.dependencies import DBDep
 
 from src.init import redis_manager
 from src.schemas.facilities import FacilityAdd
+from src.tasks.tasks import test_task
 
 router = APIRouter(prefix="/facilities", tags=["Удобства"])
 
@@ -21,5 +22,7 @@ async def get_facilities(db: DBDep):
 async def create_facility(db: DBDep, facility_data: FacilityAdd = Body()):
     facility = await db.facilities.add(facility_data)
     await db.commit()
+
+    test_task.delay()
 
     return {"status": "OK", "data": facility}
